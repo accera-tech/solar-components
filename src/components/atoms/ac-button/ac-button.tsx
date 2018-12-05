e408ddcda858e10d4c05ad5467e45d2982cbde90
Guilherme Prezzi <guilhermeprezzi1997@gmail.com>
2018-12-05 18:45:30 -0200

    feat(ac-button): Add fill 'flat' and 'clear' modes

--- src/components/atoms/ac-button/ac-button.tsx
+++ src/components/atoms/ac-button/ac-button.tsx
@@ -6,40 +6,94 @@ import { Component, Prop, Element, ComponentInterface } from '@stencil/core';
   shadow: true
 })
 export class AcButton implements ComponentInterface {
-  @Element() host: HTMLButtonElement;
+  @Element() host: HTMLAcButtonElement;
 
-  @Prop() type: string = 'button';
-  @Prop() tooltip: string;
-  @Prop() theme: string;
-  @Prop() size: string;
+  /**
+   * The HTML5 button type.
+   * See https://mdn.io/button
+   */
+  @Prop() type: 'button' | 'submit' | 'reset' = 'button';
+
+  /**
+   * The native HTML5 title attribute.
+   */
+  @Prop() tooltip?: string;
+
+  /**
+   * The theme color. The default is white.
+   */
+  @Prop() theme?: string;
+
+  /**
+   * The size of the button:
+   * * small - height: 36px
+   * * big - height: 50px
+   * * default - 44px
+   */
+  @Prop() size?: 'small' | 'big';
+
+  /**
+   * Fill mode:
+   * * flat - No borders and no raising
+   * * solid - Default raised button
+   * * clear - No background and no borders
+   */
+  @Prop() fill: 'clear' | 'solid' | 'flat' = 'solid';
+
+  /**
+   * An optional link to open when click on it.
+   * Turns the button into a anchor element.
+   */
+  @Prop() href?: string;
+
+  /**
+   * The HTML5 native anchor target to handle the href property.
+   */
+  @Prop() target?: '_blank' | '_self' | '_parent' | '_top' | string;
+
+  /**
+   * Icon only mode, with a square button, centered icon.
+   */
   @Prop() iconOnly: boolean;
+
+  /**
+   * The HTML5 native disable prop.
+   */
+  @Prop() disabled?: boolean;
 
 
   hostData() {
-    const styleModifiers = {};
-
-    if (this.theme) styleModifiers[`ac-button ac-button--${this.theme}`] = true;
-    if (this.iconOnly) styleModifiers['ac-button--icon-only'] = true;
-    if (this.size) styleModifiers[`ac-button--${this.size}`] = true;
-
     return {
       attribute: 'button',
       title: this.tooltip,
       'data-type': this.type,
-      class: styleModifiers
+      class: {
+        [`ac-button--${this.theme}`]: this.theme !== undefined,
+        [`ac-button--${this.size}`]: this.size !== undefined,
+        [`ac-button--${this.fill}`]: this.fill !== undefined,
+        [`ac-button--icon-only`]: this.iconOnly,
+        [`ac-button--disabled`]: this.disabled
+      }
     };
   }
 
   render() {
+    const TagType = this.href ? 'a' : 'button';
+    const attrs = (TagType === 'button')
+      ? { type: this.type }
+      : { href: this.href, target: this.target };
 
     return (
-      <button>
+      <TagType
+        {...attrs}
+        disabled={this.disabled}
+      >
         <slot name="icon-start" />
         <span class="ac-button__text">
           <slot />
         </span>
         <slot name="icon-end" />
-      </button>
+      </TagType>
     );
   }
 }
