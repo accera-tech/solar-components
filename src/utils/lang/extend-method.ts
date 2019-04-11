f450990ed2b89bef7dd8c32d6da53ec89e933328
Guilherme Prezzi <guilhermeprezzi1997@gmail.com>
2019-04-11 19:19:29 -0300

    fix(extend-method): Supporting multiple extends

--- src/utils/lang/extend-method.ts
+++ src/utils/lang/extend-method.ts
@@ -6,8 +6,18 @@
  * @param extend The method's patch.
  */
 export function extendMethod(target: any, method: string, extend: (original: Function, args?: Array<any>) => any) {
-  target['_' + method] = target[method];
+  let original = target[method];
+
+  target['__' + method + '__patch_list'] = target['__' + method + '__patches'] || [];
+  const patchList = target['__' + method + '__patch_list'];
+
+  patchList.push(extend);
+
   target[method] = function (...args) {
-    return extend.bind(this)(target['_' + method] ? target['_' + method].bind(this) : null, args);
+    let lastResult;
+    for (const patch of patchList) {
+      lastResult = patch.bind(this)(original ? original.bind(this) : null, args);
+    }
+    return lastResult;
   }
 }
