8e22b7da40dfc055b82e480f519116d0a530e6d0
edusiks <eduardogdusik@gmail.com>
2019-06-25 11:39:09 -0300

    feat(progress): add progress linear

---
+++ src/components/atoms/ac-progress/ac-progress.tsx
@@ -0,0 +1,34 @@
+import { Component, Prop } from '@stencil/core';
+
+@Component({
+  tag: 'ac-progress',
+  styleUrl: 'ac-progress.scss',
+  shadow: false
+})
+
+export class AcProgress {
+
+  @Prop({ reflectToAttr: true }) theme = 'primary';
+
+  @Prop({ reflectToAttr: true, mutable: true }) percent: number;
+
+  hostData() {
+    return {
+      class: {
+        [`ac-progress--${this.theme}`]: this.theme !== undefined
+      }
+    };
+  }
+
+  render() {
+    return (
+      <div
+        class="ac-progress__meter"
+        style={{
+          width: this.percent + '%',
+        }}
+      >
+      </div>
+    );
+  }
+}
