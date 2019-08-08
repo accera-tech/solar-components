e24a9740a37ea9ce5a09db514e99464508a6c0a1
Guilherme Prezzi <guilhermeprezzi1997@gmail.com>
2019-08-08 09:48:35 -0300

    feat(ac-menu): New Spec

--- src/components/molecules/ac-menu/ac-menu-item/ac-menu-item.tsx
+++ src/components/molecules/ac-menu/ac-menu-item/ac-menu-item.tsx
@@ -1,4 +1,8 @@
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
 import { Component, Prop } from '@stencil/core';
+
+import { Bind } from '../../../../utils/lang/bind';
+import { AcFaIcon } from '../../../utils/ac-fa-icon';
 
 @Component({
   tag: 'ac-menu-item',
@@ -8,26 +12,42 @@ export class AcMenu {
 
   @Prop() iconOnly: boolean;
 
+  @Prop() submenu: boolean;
+
+  @Prop({ mutable: true }) collapsed: boolean;
+
   @Prop() href: string;
 
   hostData() {
     return {
       class: {
-        'ac-menu-item--icon-only': this.iconOnly
+        'ac-menu-item--icon-only': this.iconOnly,
+        'ac-menu-item--collapsed': this.collapsed
       }
     };
   }
 
+  @Bind
+  handleClick() {
+    if (this.submenu) {
+      this.collapsed = !this.collapsed;
+    }
+  }
+
   render() {
-    return (
-      <a class="ac-menu-item__native" href={this.href}>
+    return [
+      <a class="ac-menu-item__native" href={this.href} onClick={this.handleClick}>
         <span class="ac-menu-item__icon-start">
           <slot name="icon-start" />
         </span>
         <span class="ac-menu-item__text">
           <slot />
         </span>
-      </a>
-    );
+        {this.submenu && <AcFaIcon icon={faCaretDown} class="ac-menu-item__submenu-icon" size={12} />}
+      </a>,
+      <div class="ac-menu-item__submenu">
+        <slot name="submenu" />
+      </div>
+    ];
   }
 }
