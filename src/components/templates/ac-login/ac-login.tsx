3bb633bccc34730e8c0eb2ed3ac75319000f0509
Guilherme Prezzi <guilhermeprezzi1997@gmail.com>
2019-09-03 14:25:02 -0300

    feat(login): New login template

---
+++ src/components/templates/ac-login/ac-login.tsx
@@ -0,0 +1,40 @@
+import { Component, Element, Prop } from '@stencil/core';
+import {AcNeogridShape} from "../../utils/ac-neogrid-shape";
+
+/**
+ * Accera's Login Page webcomponent.
+ */
+@Component({
+  tag: 'ac-login',
+  styleUrl: 'ac-login.scss',
+  shadow: false
+})
+export class AcLayout {
+  @Element() host;
+
+  @Prop() backgroundImageSrc: string;
+
+  render() {
+    const backgroundImageStyle = {
+      backgroundImage: `url(${this.backgroundImageSrc})`
+    };
+
+    return [
+      <div class="ac-login__background-image" style={backgroundImageStyle} />,
+      <AcNeogridShape class="ac-login__background" />,
+      <div class="ac-login__wrapper">
+        <div class="ac-login__container">
+          <div class="ac-login__header">
+            <slot name="header"/>
+          </div>
+          <div class="ac-login__content">
+            <slot name="content" />
+          </div>
+          <div class="ac-login__footer">
+            <slot name="footer" />
+          </div>
+        </div>
+      </div>
+    ];
+  }
+}
