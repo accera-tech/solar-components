0df68d835d54e270296f564d7059ef9d49596502
Guilherme Prezzi <guilhermeprezzi1997@gmail.com>
2019-11-20 11:12:34 -0300

    chore: storybook in github pages

---
+++ docs/main.ab6cb8ce04b46f79683e.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{0:function(n,o,c){c(583),c(736),n.exports=c(1354)},1:function(n,o){},736:function(n,o,c){"use strict";c.r(o);c(737),c(1166)}},[[0,1,2]]]);
--- src/components/atoms/ac-input-base/ac-input-base.stories.js
+++ src/components/atoms/ac-input-base/ac-input-base.stories.js
@@ -6,12 +6,22 @@ storiesOf('Form Field', module)
     .addDecorator(withKnobs)
     .add('Basic Input', () => {
         const label = text('Label', 'Example');
+        const size = select('Size', ['small', 'large', null], null);
         const type = select('Type', ['number', 'text', 'date', 'password'], 'date');
         const min = text('Min', '0');
         const max = text('Max', '10');
         const disabled = radios('Disabled', ['true', 'false'], 'false');
+
         return `
-        <ac-input-base type="${type}" max="${max}" min="${min}" id="inputExample" label="${label}" value="Value" disabled="${disabled}">        
+        <ac-input-base
+            type="${type}"
+            max="${max}"
+            min="${min}"
+            label="${label}"
+            value="Value"
+            disabled="${disabled}"
+            size="${size}"
+        >        
         </ac-input-base>
         `;
     }, { notes }
