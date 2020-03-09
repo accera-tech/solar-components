1bd81584824a34e660612970167c5d0d735a7b7e
Eduardo Gracez Dusik <42807647+eduardodusik@users.noreply.github.com>
2020-03-09 10:25:04 -0300

    feat(ac-text-editor): create text editor using medium editor (#111)
    * feat(ac-text-editor): create text editor using medium editor
    
    * feat(ac-text-editor): add property text to update external value
    
    * feat(ac-text-editor): override original style and improvements
    
    * feat(ac-text-editor): document prop text
    
    * feat(ac-text-editor): use promise resolve to start medium-editor

---
+++ core/src/components/molecules/ac-text-editor/ac-text-editor.tsx
@@ -0,0 +1,148 @@
+import { Component, Element, Host, Prop, State, Watch, h } from '@stencil/core';
+
+import { FormFieldBehavior, FormFieldComponent } from '../../../behaviors/form-behavior';
+import { Bind } from '../../../utils/lang/bind';
+import { CustomValidityState, ValidatorFn } from '../../../utils/validations/validations';
+import { MEDIUM_EDITOR_CDN } from '../../../vendors-cdn';
+
+@Component({
+  tag: 'ac-text-editor',
+  styleUrl: 'ac-text-editor.scss'
+})
+export class AcTextEditor implements FormFieldComponent {
+  // @ts-ignore
+  private mediumEditor: any;
+  private mediumRef: HTMLDivElement;
+
+  @Element() host: HTMLAcTextEditorElement;
+
+  /**
+   * The FormFieldBehavior instance.
+   */
+  formFieldBehavior = new FormFieldBehavior(this);
+
+  /**
+   * Get the last validity state from the checkValidity.
+   */
+  @Prop({ mutable: true }) validity: CustomValidityState;
+
+  /**
+   * The validations that this field need.
+   * This validations are checked on:
+   * - Blur event
+   * - Form submit event
+   * - Each keyUp event ONLY IF the validateOnKeyup property is present.
+   */
+  @Prop({ mutable: true }) validator: ValidatorFn | ValidatorFn[];
+
+  /**
+   * The native HTMLInputElement required attribute.
+   */
+  @Prop({ mutable: true }) required: boolean;
+
+  /**
+   * The disabled mode.
+   */
+  @Prop({ mutable: true }) disabled: boolean;
+
+  /**
+   * The HTML input field's name.
+   */
+  @Prop() name: string;
+
+  /**
+   * The value of the internal text editor.
+   */
+  @Prop() value: string;
+
+  /**
+   * The placeholder of text editor.
+   */
+  @Prop() placeholder = '';
+
+  /**
+   * Use prop text to set value.
+   */
+  @Prop() text: string;
+
+  @State() hasFocus: boolean;
+
+  componentDidLoad() {
+    this.loadMedium().then(res => {
+      // @ts-ignore
+      this.mediumEditor = new res('.ac-text-editor__editable', {
+        toolbar: {
+          buttons: ['bold', 'italic', 'underline', 'anchor', 'h2', 'h3']
+        },
+        placeholder: {
+          text: this.placeholder
+        },
+        anchor: {
+          placeholderText: 'Link',
+        }
+      });
+
+      this.mediumEditor.subscribe('editableInput', () => {
+        this.handleChange();
+      });
+    })
+  }
+
+  componentDidUnload() {
+  }
+
+  private loadMedium() {
+    return new Promise(res => {
+      const element = document.createElement('script');
+      element.setAttribute('type', 'text/javascript');
+      element.setAttribute('src', MEDIUM_EDITOR_CDN );
+      element.onload = () => res((window as any).MediumEditor);
+      (element as any).onreadystatechange = () => res((window as any).MediumEditor);
+      document.head.appendChild(element);
+    });
+  }
+
+  @Watch('text')
+  onChangeText() {
+    this.mediumRef.innerHTML = this.text;
+    this.value = this.text
+  }
+
+  @Bind
+  handleFocus() {
+    this.hasFocus = true;
+  }
+
+  @Bind
+  handleBlur() {
+    this.hasFocus = false;
+  }
+
+  @Bind
+  handleChange() {
+    this.value = this.mediumRef.innerHTML;
+  }
+
+  render() {
+    return (
+      <Host
+        class={{
+          'ac-text-editor--focus': this.hasFocus
+        }}
+      >
+        <div
+          ref={mediumRef => this.mediumRef = mediumRef as HTMLDivElement}
+          class="ac-text-editor__editable"
+          onFocus={this.handleFocus}
+          onBlur={this.handleBlur}
+          onChange={this.handleChange}
+        />
+        <input
+          name={this.name}
+          value={this.value}
+          type="hidden"
+        />
+      </Host>
+    )
+  }
+}
