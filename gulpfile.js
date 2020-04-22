bb3675863a9e2ff46c788573f64594b0cf6a4b93
Guilherme Prezzi <guilhermeprezzi1997@gmail.com>
2020-04-22 16:15:37 -0300

    chore: missing gulpfile (#123)
    * chore: enabling netlify cms
    
    * chore: updating lerna option
    
    * chore: new cms settings
    
    * chore: new cms setup
    
    * chore: adobexd preview component
    
    * chore: missing docz setting
    
    * chore: missing gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,25 @@
-const { watch, src, dest } = require('gulp');
-const inject = require('gulp-inject-string');
+const { watch, src, dest, series } = require('gulp');
 const flatten = require('gulp-flatten');
 const rename = require('gulp-rename');
 const ghpages = require('gh-pages');
 
 const docsDest = 'docs/src/pages/components';
 const mdxSrcGlob = 'core/src/**/*.{md,mdx}';
-const mdxLoader =
-`import { Playground } from 'docz';
-import { defineCustomElements } from '@accera/solar-components.core/dist/loader';
-defineCustomElements(window);
-
-`;
 
 /**
  * Copy all mdx files to docs package.
  * @param glob The glob rule to run.
+ * @param [customDest] The dest path.
  * @returns {*}
  */
-function docsCopy(glob) {
+function docsCopy(glob, customDest) {
   return src(glob)
-    .pipe(replace(
-      /^````html(.+?(?=````))````$/gms,
-      '<Playground>$1</Playground>',
-      { logs: { enabled: false } })
-    )
-    .pipe(inject.after('\n---\n', mdxLoader))
     .pipe(flatten({ includeParents: -1 }))
     .pipe(rename(path => {
       path.basename = 'code';
       path.extname = '.mdx'
     }))
-    .pipe(dest(docsDest));
+    .pipe(dest(customDest || docsDest));
 }
 
 function docsPublish() {
@@ -40,13 +28,24 @@ function docsPublish() {
   });
 }
 
-exports.docsCopy = () => docsCopy(mdxSrcGlob);
-exports.docsPublish = docsPublish;
-exports.docsWatch = () => {
+function docsWatch() {
   const watcher = watch(mdxSrcGlob);
 
-  watcher.on('change', path => docsCopy(path));
-  watcher.on('add', path => docsCopy(path));
+  watcher.on('change', docsCopyFromPath);
+  watcher.on('add', docsCopyFromPath);
 
   return watcher;
-};
+}
+
+function docsCopyAll() {
+  return docsCopy(mdxSrcGlob);
+}
+
+function docsCopyFromPath(path) {
+  const splittedPath = path.split('/');
+  return docsCopy(path, docsDest + '/', splittedPath[splittedPath.length - 2])
+}
+
+exports.docsCopy = docsCopyAll;
+exports.docsPublish = docsPublish;
+exports.docsWatch = series(docsCopyAll, docsWatch);
